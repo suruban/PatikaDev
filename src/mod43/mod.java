b6ba2e1aa9ed719d8dd9c17a2c7c7156d41a5e43
Taha Ceyhan <taha.ceyhan@outlook.com>
2025-07-16 20:42:24 +0300

    mod update

--- src/mod43/mod.java
+++ src/mod43/mod.java
@@ -1,16 +1,29 @@
 package mod43;
-
 import java.util.Scanner;
 
-public class mod  {
+public class mod {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
-        System.out.println("bir sayı giriniz: ");
-        int num = scanner.nextInt();
-        for(int i = 0; i <= num; i++){
-            if(i%4==0 && i%3==0){
-                System.out.println(i);
+
+        System.out.print("Bir sayı giriniz: ");
+        int sayi = scanner.nextInt();
+
+        int total = 0;
+        int count = 0;
+
+        for (int i = 1; i <= sayi; i++) {
+            if (i % 3 == 0 && i % 4 == 0) {
+                total += i;
+                count++;
             }
         }
+
+        if (count > 0) {
+            double ortalama = (double) total / count;
+            System.out.println("0 ile " + sayi + " arasındaki 3 ve 4'e tam bölünen sayıların ortalaması: " + ortalama);
+        } else {
+            System.out.println("0 ile " + sayi + " arasında 3 ve 4'e tam bölünen sayı yok.");
+        }
+        scanner.close();
     }
 }
